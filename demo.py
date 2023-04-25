06a05efe22de99d6666e4e67a571bb9f070120e0
Thomas Grainger <tagrain@gmail.com>
2023-04-25 16:36:28 +0100

    demonstrate with regular installs

--- demo.py
+++ demo.py
@@ -12,12 +12,19 @@ def _tmp_path():
         yield pathlib.Path(tmp_dir)
 
 
-def main():
+def demo(mode):
     with _tmp_path() as tmp_path:
         v = tmp_path / "venv"
         venv.EnvBuilder(with_pip=True).create(v)
         subprocess.run(
-            [v / "bin" / "python", "-m", "pip", "install", "-e", "src/example_setup"],
+            [
+                v / "bin" / "python",
+                "-m",
+                "pip",
+                "install",
+                *(("-e",) if mode == "editable" else ()),
+                "src/example_setup",
+            ],
             check=True,
         )
         subprocess.run(
@@ -26,21 +33,34 @@ def main():
                 "-m",
                 "pip",
                 "install",
-                "-e",
+                *(("-e",) if mode == "editable" else ()),
                 "src/example_pyproject",
             ],
             check=True,
         )
         subprocess.run(
-            [v / "bin" / "python", "-c", "import example_setup; print(example_setup.source)"],
+            [
+                v / "bin" / "python",
+                "-c",
+                "import example_setup; print(example_setup.source)",
+            ],
             check=True,
         )
-        print("=== setup.py editable succeeded ===")
+        print(f"=== setup.py {mode=} succeeded ===")
         subprocess.run(
-            [v / "bin" / "python", "-c", "import example_pyproject; print(example_pyproject.source)"],
+            [
+                v / "bin" / "python",
+                "-c",
+                "import example_pyproject; print(example_pyproject.source)",
+            ],
             check=True,
         )
-        print("=== pyproject editable succeeded ===")
+        print(f"=== pyproject {mode=} succeeded ===")
+
+
+def main():
+    demo("regular")
+    demo("editable")
 
 
 if __name__ == "__main__":
